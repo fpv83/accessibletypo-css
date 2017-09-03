3a7a635add685f281a6c59719c913ac14369d2ca
Fabian Venetz <fabian.venetz@gmail.com>
2017-09-03 23:02:46 +0200

    optimise project

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 let gulp = require('gulp');
 let cleanCSS = require('gulp-clean-css');
+let inlinesource = require('gulp-inline-source');
 
 gulp.task('default', () => {
   return gulp.src('*.css')
@@ -7,5 +8,13 @@ gulp.task('default', () => {
       console.log(details.name + ': ' + details.stats.originalSize);
       console.log(details.name + ': ' + details.stats.minifiedSize);
     }))
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('inlinesource', () => {
+  return gulp.src('index.html')
+    .pipe(inlinesource())
   .pipe(gulp.dest('docs'));
 });
+
+
